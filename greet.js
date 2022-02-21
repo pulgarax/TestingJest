d1dd87fc751a8c1d92d9c3704863151da40e3053
Axel Pulgar <brepundtrep@yahoo.com.ar>
2022-02-21 16:05:00 +0100

    testingJest

--- greet.js
+++ greet.js
@@ -1,4 +1,10 @@
 export function greet(name) {
-  // ????
-  return `Hello, ${name}!`;
+
+if (name == "Lara" || name == "Jan" || name == "CJ"){
+  return "Hello Coach!";
+} else if (name.length > 0) {
+  return "Hello " + name + "!";
+} else {
+  return "Hello stranger!";
+  }
 }
--- greet.spec.js
+++ greet.spec.js
@@ -1,9 +1,23 @@
 import { greet } from "./greet.js";
 
 describe("greet", () => {
-  it("returns …", () => {
-    // expect(greet("…")).toBe(…)
+  it("returns 'Hello Coach!'", () => {
+     expect(greet("CJ")).toBe("Hello Coach!");
   });
 
-  // add more test here!
-});
+  it("returns 'Hello Coach!'", () => {
+    expect(greet("Lara")).toBe("Hello Coach!");
+  });
+
+  it("returns 'Hello Coach!'", () => {
+    expect(greet("Jan")).toBe("Hello Coach!");
+  });
+
+  it("returns 'Hello Coach!'", () => {
+    expect(greet("Axel")).toBe("Hello Axel!");
+  });
+
+  it("returns 'Hello stranger!'", () => {
+    expect(greet("")).toBe("Hello stranger!");
+  });
+ });
